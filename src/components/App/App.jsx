844b7ec80c34f39d2d6d1bd45eeba7ddbb097565
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2023-11-16 10:47:04 +0100

    imports
    adding several imports

---
+++ src/components/App/App.jsx
@@ -0,0 +1,22 @@
+import React, { lazy } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+// const App = () => {
+//   return (
+//     <div>
+//       <Routes>
+//         <Route path="/" element={<SharedLayout />}>
+//           <Route index element={<Home />} />
+//           <Route path="movies" element={<Movies />} />
+//           <Route path="movies/:movieId" element={<MovieDetails />}>
+//             <Route path="cast" element={<Cast />} />
+//             <Route path="reviews" element={<Reviews />} />
+//           </Route>
+//           <Route path="*" element={<NotFound />} />
+//         </Route>
+//       </Routes>
+//     </div>
+//   );
+// };
+
+// export default App;

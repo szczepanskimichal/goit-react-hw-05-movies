69df1476a0349c5a84a6d919050857944685fa39
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2023-11-19 06:54:56 +0100

    Update Home.jsx
    uncommenting the code

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,44 +1,44 @@
-// import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
-// import { getTrendingMovies } from '../../services/moviesAPI';
-// import { Loader, MovieList } from '../../components';
+import { getTrendingMovies } from '../../services/moviesAPI';
+import { Loader, MovieList } from '../../components';
 
-// const Home = () => {
-//   const [movies, setMovies] = useState([]);
-//   const [error, setError] = useState(null);
-//   const [isLoading, setIsLoading] = useState(false);
+const Home = () => {
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
-//   useEffect(() => {
-//     const showMovies = async () => {
-//       setIsLoading(true);
-//       try {
-//         const data = await getTrendingMovies();
-//         setMovies(data);
-//       } catch (error) {
-//         setError(error);
-//       } finally {
-//         setIsLoading(false);
-//       }
-//     };
-//     showMovies();
-//   }, []);
+  useEffect(() => {
+    const showMovies = async () => {
+      setIsLoading(true);
+      try {
+        const data = await getTrendingMovies();
+        setMovies(data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    showMovies();
+  }, []);
 
-//   if (isLoading) {
-//     return (
-//       <div>
-//         <Loader />
-//       </div>
-//     );
-//   } else if (error) {
-//     return <p>Something went wrong: {error.message}</p>;
-//   } else {
-//     return (
-//       <div>
-//         <h2>Trending today</h2>
-//         <MovieList movies={movies} />
-//       </div>
-//     );
-//   }
-// };
+  if (isLoading) {
+    return (
+      <div>
+        <Loader />
+      </div>
+    );
+  } else if (error) {
+    return <p>Something went wrong: {error.message}</p>;
+  } else {
+    return (
+      <div>
+        <h2>Trending today</h2>
+        <MovieList movies={movies} />
+      </div>
+    );
+  }
+};
 
-// export default Home;
+export default Home;

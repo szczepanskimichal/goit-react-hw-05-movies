fd1464b4292e7de3903eb6bcf0df5213d070684f
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2023-11-22 07:19:22 +0100

    File repair SearchBar.jsx & .css

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,3 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { BiSearchAlt2 } from 'react-icons/bi';
+
+import css from './SearchBar.module.css';
+
+export const SearchBar = ({ onChange, value }) => {
+  return (
+    <div className={css['Searchbar']}>
+      <BiSearchAlt2 size="16" className={css['Searchbar-icon']} />
+      <input
+        className={css['Searchbar-input']}
+        type="text"
+        autoComplete="off"
+        autoFocus
+        placeholder="Search movies"
+        onChange={onChange}
+        value={value}
+      />
+    </div>
+  );
+};
+
+SearchBar.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};

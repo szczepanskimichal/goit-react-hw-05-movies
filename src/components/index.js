856ba3dc5fc5f5d8d148a056bfc240c7efb0b7d5
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2023-11-23 06:51:46 +0100

    delete MovieCard folder, update ./components/index.js file

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,6 @@ export * from './BackLink';
 export * from './Cast';
 export * from './Reviews';
 export * from './Loader';
-export * from './MovieCard';
 export * from './MovieList';
 export * from './SearchBar';
 export * from './SharedLayout';
